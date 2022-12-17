e2b98ed6b1aac5344e6bf937c2a68dbd2e74771b
gitdjay <tiwaridhananjay70@gmail.com>
2022-12-17 21:22:02 +0530

    Token Transfer Delegation

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -24,14 +24,21 @@ contract Token {
 
 	function transfer(address _to, uint256 _value) public returns (bool success) {
 		require(balanceOf[msg.sender] >= _value);
-		require(_to != address(0));
+		
+		_transfer(msg.sender, _to, _value);
 
-		balanceOf[msg.sender] = balanceOf[msg.sender] - _value;
-		balanceOf[_to] = balanceOf[_to] + _value;
-
-		emit Transfer(msg.sender, _to, _value);
 		return true;
 	}
+
+	function _transfer(address _from, address _to, uint256 _value) internal {
+		require(_to != address(0));
+
+		balanceOf[_from] = balanceOf[_from] - _value;
+		balanceOf[_to] = balanceOf[_to] + _value;
+
+		emit Transfer(_from , _to, _value);
+	}
+
 
 	function approve(address _spender, uint256 _value) public returns (bool success)
 	{
@@ -39,6 +46,17 @@ contract Token {
 		allowance[msg.sender][_spender] = _value;
 
 		emit Approval(msg.sender, _spender, _value);
+		return true;
+	}
+
+	function transferFrom(address _from, address _to, uint256 _value) public returns (bool success)
+	{
+		require(_value <= balanceOf[_from]);
+		require(_value <= allowance[_from][msg.sender]);
+
+		allowance[_from][msg.sender] = allowance[_from][msg.sender] - _value;
+
+		_transfer(_from, _to, _value);
 		return true;
 	}
 }
